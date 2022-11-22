ca1e31b55e63eda6db1455bfde21c8b90a5a0ea5
RegentUser <113810330+RegentUser@users.noreply.github.com>
2022-11-22 12:02:34 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import {Button ,StyleSheet, Text, TextInput, View } from "react-native";
+
+export default function App() {
+  const [objeto,setObjeto] = useState('')
+  const [nome, setNome] = useState('')
+  const [idade, setIdade] = useState('')
+  const [auxIdade, setAuxIdade] = useState('')
+  const [auxMarca, setMarca] = useState('')
+useEffect ( () => trocarValores(), [nome])
+
+  function trocarValores(){
+  setNome(objeto)
+  setIdade(auxIdade)
+  setMarca(auxMarca)
+}
+  
+return (
+
+  <View sytle={styles.container}>
+  <Text>Valores com a state e effect</Text>
+  <TextInput
+  placeholder="Digite o nome do objeto: "
+  value={objeto}
+  onChangeText={(texto) => setObjeto(texto) }
+/>
+<TextInput placeholder='Digite sua idade:' 
+value={auxIdade}
+onChangeText={(texto) => setAuxIdade(texto)}
+/>
+<TextInput
+  placeholder="Digite o nome da marca: "
+  value={auxMarca}
+  onChangeText={(texto) => setMarca(texto) }
+/>
+
+  <Button title="Enviar" onPress={ () => trocarValores(objeto)} 
+  />
+
+  <Text>{ nome }</Text>
+  <Text>{idade}</Text>
+  <Text>{auxMarca}</Text>
+</View>
+);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent:'center',
+
+  },
+});
+
